b793765eba9288f3fa4444c22d01c08ee2322da2
fathur <fathur_rohman17@yahoo.co.id>
2015-06-02 14:13:32 +0700

    about us

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+ 
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'public/css/lautan-luas-bootstrap.css': 'resources/scss/main.scss',
+				}
+			},
+			production: {
+				options: {
+					style: 'compressed'
+				},
+				
+				files: {
+					'public/css/lautan-luas-bootstrap.min.css': 'resources/scss/main.scss',
+				}
+			}
+		},
+
+		jade: {
+		  compile: {
+		    options: {
+		      pretty: true,
+		      client: false
+		    },
+		    files: [{
+		      "public/index.html": "resources/jade/index.jade",
+		      "public/csr.html": "resources/jade/csr.jade",
+		      "public/contact-us.html": "resources/jade/contact-us.jade",
+		      "public/careers.html": "resources/jade/careers.jade",
+		      "public/about-us.html": "resources/jade/about-us.jade",
+		      "public/industries.html": "resources/jade/industries.jade",
+		      "public/investor-relations.html": "resources/jade/investor-relations.jade"
+		    }]
+		  }
+		},
+
+
+		/**
+		 * Autorunt task dan mendeteksi perubahan tanpa harus
+		 * mengulangi menjalankan perintah grunt
+		 */
+		watch: {
+			/*options: {
+				livereload: 3000
+			},*/
+			sass: {
+				files: ['resources/scss/**/*.scss'],
+				tasks: ['sass']
+			},
+			jade: {
+				files: ['resources/jade/**/*.jade'],
+				tasks: ['jade']
+			}
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+
+	grunt.registerTask('default', [
+		'sass',
+		'jade',
+		'watch',
+	]);
+	
+}
